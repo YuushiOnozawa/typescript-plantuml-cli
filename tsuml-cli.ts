5a14723cab989916bffe262373eb0de04ae4ec1b
YuushiOnozawa <31531668+YuushiOnozawa@users.noreply.github.com>
2018-01-05 18:20:56 +0900

    change caporal

--- tsuml-cli.ts
+++ tsuml-cli.ts
@@ -1,38 +1,49 @@
 const ts = require('typescript');
 const tsuml = require('typescript-uml');
-const program = require('commander');
+const program = require('caporal');
 const glob = require('glob');
 const fs = require('fs-extra');
 const path = require('path');
 
 program
-  .option('-f, --files <items>', 'file list')
-  .option('-o, --out <out>', 'output dir')
-  .parse(process.argv);
-
-const fileList: string[] = glob.sync(program.files);
-
-fileList.forEach((item: string) => {
-  const tmpPath = path.parse(item);
-  const outDir: string = path.join(process.cwd(), program.out, tmpPath.dir);
-  const outFile: string = path.join(outDir, tmpPath.name + '.puml');
-  fs.outputFile(outFile,
-    getPlantUmlData(item),
-    (err: Error) => {
-    if(err) {
-      return console.log(err);
+  .option('--files <items>', 'file list')
+  .option('--out <out>', 'output dir')
+  .action((arg: any, option: any) => {
+    // validation
+    if(!option.files) {
+      console.error('no file input');
+      process.exit(1);
     }
-  });
-});
+    if(!option.out){
+      console.error('no output dir')
+      process.exit(1);
+    }
+    const fileList: string[] = glob.sync(option.files);
+    fileList.forEach((item: string) => {
+      const tmpPath = path.parse(item);
+      const outDir: string = path.join(process.cwd(), option.out, tmpPath.dir);
+      const outFile: string = path.join(outDir, tmpPath.name + '.puml');
 
-function getPlantUmlData(filepath: string) {
-  return tsuml.TypeScriptUml.generateClassDiagram(
-    tsuml.TypeScriptUml.parseFile(filepath, ts.ScriptTarget.ES5),
-    {
-      formatter: 'plantuml',
-      plantuml: {
-        diagramTags: true,
-      },
-    },
-  )
-}
+      fs.outputFile(outFile,
+        getPlantUmlData(item),
+        (err: Error) => {
+          if(err) {
+            return console.log(err);
+          }
+        });
+    });
+
+    function getPlantUmlData(filepath: string) {
+      return tsuml.TypeScriptUml.generateClassDiagram(
+        tsuml.TypeScriptUml.parseFile(filepath, ts.ScriptTarget.ES5),
+        {
+          formatter: 'plantuml',
+          plantuml: {
+            diagramTags: true,
+          },
+        },
+      )
+    }
+
+  })
+program.parse(process.argv)
