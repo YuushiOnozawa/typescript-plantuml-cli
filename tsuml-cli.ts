a4063b91bc19d129598ea7eb09447a8c08d6cfdc
YuushiOnozawa <31531668+YuushiOnozawa@users.noreply.github.com>
2018-01-05 14:17:31 +0900

    output pumlfiles

--- tsuml-cli.ts
+++ tsuml-cli.ts
@@ -2,23 +2,37 @@ const ts = require('typescript');
 const tsuml = require('typescript-uml');
 const program = require('commander');
 const glob = require('glob');
+const fs = require('fs-extra');
+const path = require('path');
 
 program
-  .option('--files <items>', 'file list')
+  .option('-f, --files <items>', 'file list')
+  .option('-o, --out <out>', 'output dir')
   .parse(process.argv);
+
 const fileList: string[] = glob.sync(program.files);
+
 fileList.forEach((item: string) => {
-  console.log(
-    tsuml.TypeScriptUml.generateClassDiagram(
-      tsuml.TypeScriptUml.parseFile(item, ts.ScriptTarget.ES5),
-      {
-        formatter: 'plantuml',
-        plantuml: {
-          diagramTags: false,
-        },
-      },
-    )
-  );
+  const tmpPath = path.parse(item);
+  const outDir: string = path.join(process.cwd(), program.out, tmpPath.dir);
+  const outFile: string = path.join(outDir, tmpPath.name + '.puml');
+  fs.outputFile(outFile,
+    getPlantUmlData(item),
+    (err: Error) => {
+    if(err) {
+      return console.log(err);
+    }
+  });
 });
 
-
+function getPlantUmlData(filepath: string) {
+  return tsuml.TypeScriptUml.generateClassDiagram(
+    tsuml.TypeScriptUml.parseFile(filepath, ts.ScriptTarget.ES5),
+    {
+      formatter: 'plantuml',
+      plantuml: {
+        diagramTags: true,
+      },
+    },
+  )
+}
